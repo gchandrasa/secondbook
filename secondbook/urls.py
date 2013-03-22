09996f97e68635503500f2a8e0cf400fa1d202bd
Gilang Chandrasa <gilang.chandrasa@gmail.com>
2013-03-22 23:08:07 +0700

    Add landing page

--- secondbook/urls.py
+++ secondbook/urls.py
@@ -1,17 +1,15 @@
 from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.conf import settings
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'secondbook.views.home', name='home'),
-    # url(r'^secondbook/', include('secondbook.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('website.urls', namespace='website')),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT}),
+    )
