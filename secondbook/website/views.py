acd31f41d597f78a1d44806690196914cf864c14
Gilang Chandrasa <gilang.chandrasa@gmail.com>
2013-03-31 03:35:08 +0700

    Add book list on homepage

--- secondbook/website/views.py
+++ secondbook/website/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 
 from profiles.views import home
+from books.models import Book
 
 
 def index(request):
 
     if request.user.is_authenticated():
         return home(request)
-    return render(request, 'index.html')
+    book_list = Book.objects.all()[:12]
+    context = {
+        'book_list': book_list
+    }
+    return render(request, 'index.html', context)
